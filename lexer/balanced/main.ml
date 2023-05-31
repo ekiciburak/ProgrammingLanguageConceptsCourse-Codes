5e9720b862a48a4599e6000560c1734d3055cc7e
ekiciburak <ekcburak@hotmail.com>
2023-05-31 09:52:02 +0300

    cleanup...

--- lexer/balanced/main.ml
+++
@@ -1,9 +0,0 @@
-open Printf
-open Lexing
-open Lexer
-
-let main: unit =
-  let t = tokenize (from_string "[[]]burak") in
-  printToken t;
-  let tl = getTokensFromString "][2023[]]burak" in
-  printTokenList tl;
